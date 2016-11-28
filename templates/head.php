95de475c15d19bc1df0ea13726a24608acfb8d18
Ariflaw <nurariflaw@gmail.com>
2016-11-29 06:30:19 +0700

    Update

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,6 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
+    <!-- Font Include -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i,900|PT+Serif:400,400i,700,700i" rel="stylesheet">
 </head>
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,28 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header id="hero" class="hero_header">
+    <div class="container">
+        <div class="main_header clearfix">
+            <div class="brand">
+                <a class="logo" href="<?php esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/Logo.png" /></a>
+            </div>
+            <nav class="nav_primary">
+                <?php
+                if (has_nav_menu('primary_navigation')) :
+                    wp_nav_menu([
+                        'theme_location' => 'primary_navigation',
+                        'menu_class'     => 'nav'
+                    ]);
+                endif;
+                ?>
+            </nav>
+        </div>
+
+        <div class="hero_content">
+            <h1 class="page_title">Blog</h1>
+            <p class="page_des">What I think here.</p>
+        </div>
+
+        <div class="hero_profile">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/profile.png" alt="Avatar">
+        </div>
+    </div>
 </header>
